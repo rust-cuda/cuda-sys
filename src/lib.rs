ac79fc15a6b6dca3659c564a5a1af811a306b0bd
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-01-07 12:15:42 +0900

    Drop old binding (CUDA 8-based)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 #![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
-include!("bindgen/cuda.rs");
-include!("bindgen/cucomplex.rs");
-include!("bindgen/vector_types.rs");
-include!("bindgen/library_types.rs");
+
+pub mod driver {
+    include!("bindgen/cuda.rs");
+    include!("bindgen/cucomplex.rs");
+    include!("bindgen/vector_types.rs");
+    include!("bindgen/library_types.rs");
+}
+
+pub mod runtime {
+    use driver::*;
+    type CUexternalMemory_st = CUexternalMemory;
+    type CUexternalSemaphore_st = CUexternalSemaphore;
+    include!("bindgen/cuda_runtime.rs");
+}
