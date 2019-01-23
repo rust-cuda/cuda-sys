b5e1a6ed5c78e471ed4ec7e27010a3dc4b7e4819
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-01-24 01:32:58 +0900

    Revert "Fix"
    This reverts commit d3346350a4969a32441db9f1e27c71ca065f8db0.

--- build.rs
+++ build.rs
@@ -35,6 +35,13 @@ fn find_cuda() -> Vec<PathBuf> {
             valid_paths.push(lib.clone());
             valid_paths.push(lib.join("stubs"));
         }
+        let base = base.join("targets/x86_64-linux");
+        let header = base.join("include/cuda.h");
+        if header.is_file() {
+            valid_paths.push(base.join("lib"));
+            valid_paths.push(base.join("lib/stubs"));
+            continue;
+        }
     }
     eprintln!("Found CUDA paths: {:?}", valid_paths);
     valid_paths
@@ -112,9 +119,9 @@ fn main() {
             println!("cargo:rustc-link-search=native={}", path.display());
         }
     };
+    println!("cargo:rustc-link-lib=dylib=cuda");
     println!("cargo:rustc-link-lib=dylib=cudart");
     println!("cargo:rustc-link-lib=dylib=cublas");
-    println!("cargo:rustc-link-lib=dylib=cuda");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=CUDA_LIBRARY_PATH");
 }
