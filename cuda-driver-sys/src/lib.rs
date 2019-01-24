513104ee87453da57c6be12586cd1f839a792194
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-01-25 05:33:01 +0900

    Add cuda-driver-sys

---
+++ cuda-driver-sys/src/lib.rs
@@ -0,0 +1,6 @@
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+
+include!("cuda.rs");
+include!("cucomplex.rs");
+include!("vector_types.rs");
+include!("library_types.rs");
