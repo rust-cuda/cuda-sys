0b4e3440dc318725505694122273a85c8f7c20a7
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-01-25 05:59:57 +0900

    cuda-searcher, search where CUDA is installed

--- cuda-searcher/src/lib.rs
+++ cuda-searcher/src/lib.rs
@@ -1,4 +1,3 @@
-extern crate glob;
 
 use glob::glob;
 use std::{env, path::PathBuf};
@@ -18,7 +17,7 @@ fn read_env() -> Vec<PathBuf> {
     }
 }
 
-fn find_cuda() -> Vec<PathBuf> {
+pub fn find_cuda() -> Vec<PathBuf> {
     let mut candidates = read_env();
     candidates.push(PathBuf::from("/opt/cuda"));
     candidates.push(PathBuf::from("/usr/local/cuda"));
@@ -47,7 +46,7 @@ fn find_cuda() -> Vec<PathBuf> {
     valid_paths
 }
 
-fn find_cuda_windows() -> PathBuf {
+pub fn find_cuda_windows() -> PathBuf {
     let paths = read_env();
     if !paths.is_empty() {
         return paths[0].clone();
@@ -106,22 +105,4 @@ fn find_cuda_windows() -> PathBuf {
 
     // No idea where to look for CUDA
     panic!("CUDA cannot find");
-}
-
-fn main() {
-    if cfg!(target_os = "windows") {
-        println!(
-            "cargo:rustc-link-search=native={}",
-            find_cuda_windows().display()
-        );
-    } else {
-        for path in find_cuda() {
-            println!("cargo:rustc-link-search=native={}", path.display());
-        }
-    };
-    println!("cargo:rustc-link-lib=dylib=cuda");
-    println!("cargo:rustc-link-lib=dylib=cudart");
-    println!("cargo:rustc-link-lib=dylib=cublas");
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-env-changed=CUDA_LIBRARY_PATH");
 }
