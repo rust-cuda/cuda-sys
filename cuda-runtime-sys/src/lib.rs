ee8398ccf8cfa9b760bd6b7e00cbf00eb6127b34
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-01-25 05:36:57 +0900

    Add cuda-runtime-sys

--- cuda-runtime-sys/src/lib.rs
+++ cuda-runtime-sys/src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+
+extern crate cuda_driver_sys as driver;
+
+use driver::*;
+type CUexternalMemory_st = CUexternalMemory;
+type CUexternalSemaphore_st = CUexternalSemaphore;
+include!("cuda_runtime.rs");
