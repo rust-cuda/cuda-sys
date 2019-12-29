c1ff456baa8c3ad101ee4356f9084e36bcde8afd
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-12-30 08:46:11 +0900

    Create cuda-config

--- cublas-sys/build.rs
+++ cublas-sys/build.rs
@@ -1,1 +1,4 @@
-fn main() {}
+fn main() {
+    println!("cargo:rustc-link-lib=dylib=cublas");
+    println!("cargo:rerun-if-changed=build.rs");
+}
--- cuda-driver-sys/build.rs
+++ cuda-driver-sys/build.rs
@@ -1,1 +1,4 @@
-fn main() {}
+fn main() {
+    println!("cargo:rustc-link-lib=dylib=cuda");
+    println!("cargo:rerun-if-changed=build.rs");
+}
