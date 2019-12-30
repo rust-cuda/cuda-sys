657783846777402745e39af0cd42105e701395d3
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-12-30 20:56:34 +0900

    Add link test using cudaDriverGetVersion

--- cuda-driver-sys/src/lib.rs
+++ cuda-driver-sys/src/lib.rs
@@ -3,8 +3,15 @@ include!("cuda.rs");
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn get_version() {
+        let mut version: i32 = 0;
+        let result = unsafe { cuDriverGetVersion(&mut version as *mut i32) };
+        if result != CUresult::CUDA_SUCCESS {
+            panic!("Cannot get driver version: ERROR={:?}", result);
+        }
+        println!("Version = {}", version);
     }
 }
